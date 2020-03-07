c0a5c2cfdd4bff50e142836fee961c7dc7fc82cc
Thomas Leyh <thomas.leyh@mailbox.org>
2020-03-07 19:22:52 +0900

    Perspective transform of matching images

--- urls.py
+++ urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.TitleList.as_view(), name='titles'),
     path('book/<int:pk>', views.TitlesBookList.as_view(), name='books'),
     path('pages/<int:pk>', views.BookPagesView.as_view(), name='pages'),
-    path('page/<int:pk>', views.PageView.as_view(), name='page')
+    path('page/<int:pk>', views.PageView.as_view(), name='page'),
+    path('match/<int:src_page_id>/<int:dst_page_id>', views.matching_image, name='match')
 ]
--- views.py
+++ views.py
@@ -1,7 +1,13 @@
 from django.db.models import Count
 from django.views import generic
+from django.http import FileResponse
+from django.contrib.staticfiles import finders
 
-from .models import Title, Book, Page
+from .models import Title, Book, Page, Match
+
+import io
+import cv2 as cv
+import numpy as np
 
 
 PAGE_FILENAME_TEMPLATE = "{book_id}/{book_id}_{page:0>5}_{lr}.jpg"
@@ -67,3 +73,32 @@ class PageView(generic.DetailView):
         context['matching'] = union
         context['filename'] = PAGE_FILENAME_TEMPLATE.format(book_id=page.book_id, page=page.page, lr=page.lr)
         return context
+
+
+def get_keypoints(src_page: Page, dst_page: Page):
+    src_dst_matches = Match.objects.filter(src_keypoint__page=src_page).filter(dst_keypoint__page=dst_page)
+    dst_src_matches = Match.objects.filter(src_keypoint__page=dst_page).filter(dst_keypoint__page=src_page)
+    assert (src_dst_matches.count() == 0) or (dst_src_matches.count() == 0)
+    if src_dst_matches.count != 0:
+        return src_dst_matches.values_list('src_keypoint__x', 'src_keypoint__y', 'dst_keypoint__x', 'dst_keypoint__y')
+    else:
+        return src_dst_matches.values_list('dst_keypoint__x', 'dst_keypoint__y', 'src_keypoint__x', 'src_keypoint__y')
+
+
+def matching_image(response, src_page_id, dst_page_id):
+    src_page = Page.objects.get(pk=src_page_id)
+    dst_page = Page.objects.get(pk=dst_page_id)
+    dst_filename = PAGE_FILENAME_TEMPLATE.format(book_id=dst_page.book_id, page=dst_page.page, lr=dst_page.lr)
+    dst_path = finders.find('images/' + dst_filename)
+    dst_image = cv.imread(dst_path, cv.IMREAD_GRAYSCALE)
+    keypoints = np.array(get_keypoints(src_page, dst_page))
+    homography, _ = cv.findHomography(keypoints[:,2:], keypoints[:,:2], 0)
+    height, width = dst_image.shape
+    dst_image_warped = cv.warpPerspective(dst_image, homography, (width, height))
+    enc_result, dst_image_warped_jpg = cv.imencode(".jpg", dst_image_warped, [cv.IMWRITE_JPEG_QUALITY, 80])
+    assert enc_result
+    r = FileResponse(io.BytesIO(dst_image_warped_jpg))
+    r['Content-Disposition'] = f'inline; filename="{dst_filename}"'
+    r['Content-Type'] = 'image/jpeg'
+    r['Content-Length'] = len(dst_image_warped_jpg)
+    return r
